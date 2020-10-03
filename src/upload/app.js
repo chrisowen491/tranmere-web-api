8181b3cfe322db0d062a6076ce68c48697a0a743
chrisowen <chrisowen491@gmail.com>
2020-10-03 23:06:05 +0100

    updates

--- src/upload/app.js
+++ src/upload/app.js
@@ -4,17 +4,18 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context) => {
+exports.mediaHandler = (event, context) => {
    console.log("Incoming Event: ", event);
    const bucket = event.Records[0].s3.bucket.name;
    const filename = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
    const params = { Bucket: bucket, Key: filename };
+   console.log(params);
    const s3Stream = s3.getObject(params).createReadStream()
 
     csv().fromStream(s3Stream)
          .on('data', (row) => {
             let item = JSON.parse(row);
-            console.log(JSON.stringify(jsonContent));
+            console.log(JSON.stringify(item));
             item.id = uuidv4();
             let paramsToPush = {
                 TableName:item.TableName,
