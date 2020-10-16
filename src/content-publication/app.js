f7c236793ec53a80481931931c73d7a7f7132af7
chrisowen <chrisowen491@gmail.com>
2020-10-16 19:13:21 +0100

    updates

--- src/content-publication/app.js
+++ src/content-publication/app.js
@@ -1,5 +1,23 @@
+const { v4: uuidv4 } = require('uuid');
+const AWSXRay = require('aws-xray-sdk');
+const AWS = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
+AWSXRay.captureAWSClient(dynamo.service);
+const TABLE_NAME = 'TranmereWebPlayerTable';
+
 exports.handler = async function (event, context) {
     console.log('Received event:', event);
+
+    if(event.body) {
+        var body = JSON.parse(event.body)
+        if(body.sys.ContentType.sys.id === "player" && body.sys.revision = 1) {
+            var player = constructPlayer(body);
+            await insertPlayer(player);
+        } else if(body.sys.ContentType.sys.id === "player") {
+            var player = constructPlayer(body);
+            await updatePlayer(player);
+        }
+    }
 
     return {
      "isBase64Encoded": false,
@@ -8,3 +26,72 @@ exports.handler = async function (event, context) {
      "body": JSON.stringify({message: "ok"})
      };
 };
+
+function constructPlayer(obj) {
+    var player = {
+        name: obj.fields.name["en-US"],
+        dateOfBirth: obj.fields.dateOfBirth["en-US"],
+        id: uuidv4(),
+        profile: JSON.stringify(obj.fields.biography["en-US"].content)
+    };
+    return player;
+}
+
+function insertPlayer(player){
+	const params = {
+		TableName: TABLE_NAME,
+		Item: player
+	};
+
+	return dynamo
+		.put(params)
+		.promise()
+		.then((result) => {
+			return item;
+		}, (error) => {
+			return error;
+		});
+}
+
+function updatePlayer(player){
+
+	let vbl = "x";
+	let adder = "y";
+	let updateexp = 'set ';
+	let itemKeys =  Object.keys(player);
+	let expattvalues = {};
+
+	for (let i = 0; i < itemKeys.length; i++) {
+		vbl = vbl+adder;
+
+		if((itemKeys.length-1)==i)
+			updateexp += itemKeys[i] + ' = :'+ vbl;
+		else
+			updateexp += itemKeys[i] + ' = :'+ vbl + ", ";
+
+		expattvalues[":"+vbl] = player[itemKeys[i]];
+	}
+
+	console.log("update expression and expressionAttributeValues");
+	console.log(updateexp, expattvalues);
+
+	const params = {
+		TableName: TABLE_NAME,
+		Key: {
+			name: player.name
+		},
+		ConditionExpression: 'attribute_exists(playerId)',
+		UpdateExpression: updateexp,
+		ExpressionAttributeValues: expattvalues,
+		ReturnValues: 'ALL_NEW'
+	};
+
+	return dynamo
+		.update(params)
+		.promise()
+		.then(response => {
+			return response.Attributes;
+		}, (error) => {
+			return error;
+		});
+}
