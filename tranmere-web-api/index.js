3c4e4d34f9c6436e4b1bfa37e6e9524eda9a9d9a
chrisowen <chrisowen491@gmail.com>
2020-10-18 17:30:23 +0100

    update

--- tranmere-web-api/index.js
+++ tranmere-web-api/index.js
@@ -1,6 +1,14 @@
 addEventListener("fetch", (event) => {
   let url = new URL(event.request.url)
-  url.host = "api.tranmere-web.com"
+
+  if(url.pathname === "/" || url.pathname.startsWith("/page") || url.pathname.startsWith("/player-search")|| url.pathname.startsWith("/result-search")) {
+    url.host = "api.tranmere-web.com"
+    if(url.pathname === "/") {
+        url.pathname = "/page/home/home"
+    }
+  }
+
+
   let request = new Request(url, event.request)
   request.headers.set("x-api-key", APIKEY)
   event.respondWith(
@@ -8,7 +16,7 @@ addEventListener("fetch", (event) => {
       cf: {
         cacheEverything: true,
         cacheTtlByStatus: {
-          "200-299": 86400,
+          "200-299": 172800,
           404: 1,
           "500-599": 0
         }
