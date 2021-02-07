ad8d2b210412eb16465984bdd2d6f77ba55c1dbd
chrisowen <chrisowen491@gmail.com>
2021-02-07 12:32:52 +0000

    Add

--- tranmere-web-api/index.js
+++ tranmere-web-api/index.js
@@ -8,6 +8,27 @@ addEventListener("fetch", (event) => {
     }
   }
 
+  if(url.pathname === "/media.html") {
+    url.pathname = "/page/blog/7wtrOLaYqaK7Dhvodz1Gv0";
+    url.host = "api.tranmere-web.com";
+  }
+
+
+  if(url.pathname === "/about.html") {
+    url.pathname = "/page/blog/2oXtDQNbfluRQk5jWbyCu7";
+    url.host = "api.tranmere-web.com";
+  }
+
+
+  if(url.pathname === "/super-stars.html") {
+    url.pathname = "/page/blog/4EZzxJSam4dohEgD1sOmki";
+    url.host = "api.tranmere-web.com";
+  }
+
+  if(url.pathname === "/stats.html") {
+    url.pathname = "/page/blog/2VrsLTKALyi2vgAQMLDoIT";
+    url.host = "api.tranmere-web.com";
+  }
 
   let request = new Request(url, event.request)
   request.headers.set("x-api-key", APIKEY)
