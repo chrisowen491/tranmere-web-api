8c73f8796864ffa0f93810dde62ccd92c298f25f
chrisowen <chrisowen491@gmail.com>
2021-02-12 18:26:29 +0000

    updates

---
+++ src/edge/request.js
@@ -0,0 +1,21 @@
+exports.handler = (event, context, callback) => {
+     const request = event.Records[0].cf.request;
+
+     if (request.uri.startsWith("/page") || request.uri.startsWith("/player-search") || request.uri.startsWith("/result-search")) {
+         /* Set custom origin fields*/
+         request.origin = {
+             custom: {
+                 domainName: 'api.tranmere-web.com',
+                 port: 443,
+                 protocol: 'https',
+                 path: '',
+                 sslProtocols: ['TLSv1', 'TLSv1.1'],
+                 readTimeout: 5,
+                 keepaliveTimeout: 5,
+                 customHeaders: {}
+             }
+         };
+         request.headers['host'] = [{ key: 'host', value: 'api.tranmere-web.com'}];
+    }
+    callback(null, request);
+};
