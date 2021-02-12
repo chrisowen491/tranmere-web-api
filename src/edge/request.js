3f4d01e40766ea697c6397f2d9c38811864adb90
chrisowen <chrisowen491@gmail.com>
2021-02-12 20:55:03 +0000

    updates

--- src/edge/request.js
+++ src/edge/request.js
@@ -1,23 +1,25 @@
 exports.handler = (event, context, callback) => {
      const request = event.Records[0].cf.request;
 
-     if (request.uri.startsWith("/page") || (request.uri.startsWith("/player-search") && !request.uri.startsWith("/player-search.html")) || request.uri.startsWith("/result-search")) {
-         /* Set custom origin fields*/
-         request.origin = {
-             custom: {
-                 domainName: 'api.tranmere-web.com',
-                 port: 443,
-                 protocol: 'https',
-                 path: '',
-                 sslProtocols: ['TLSv1', 'TLSv1.1'],
-                 readTimeout: 5,
-                 keepaliveTimeout: 5,
-                 customHeaders: {
+     if (request.uri.startsWith("/page") || (request.uri.startsWith("/player-search") || request.uri.startsWith("/result-search")) {
+         if(!request.uri.startsWith("/player-search.html")) {
+             /* Set custom origin fields*/
+             request.origin = {
+                 custom: {
+                     domainName: 'api.tranmere-web.com',
+                     port: 443,
+                     protocol: 'https',
+                     path: '',
+                     sslProtocols: ['TLSv1', 'TLSv1.1'],
+                     readTimeout: 5,
+                     keepaliveTimeout: 5,
+                     customHeaders: {
+                     }
                  }
-             }
-         };
-         request.headers['x-api-key'] = [{ key: 'x-api-key', value: 'Ubz2w38CTS18anpiEApqf1pBWayHRcmLz5fKyyW4'}];
-         request.headers['host'] = [{ key: 'host', value: 'api.tranmere-web.com'}];
+             };
+             request.headers['x-api-key'] = [{ key: 'x-api-key', value: 'Ubz2w38CTS18anpiEApqf1pBWayHRcmLz5fKyyW4'}];
+             request.headers['host'] = [{ key: 'host', value: 'api.tranmere-web.com'}];
+         }
     }
     callback(null, request);
 };
