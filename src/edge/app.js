fb1f02cf6f204bacf7720a2e3428319a1ff72f41
chrisowen <chrisowen491@gmail.com>
2021-02-12 20:24:19 +0000

    updates

--- src/edge/app.js
+++ src/edge/app.js
@@ -1,29 +1,9 @@
-const https = require('https');
-
 exports.handler = async function (event, context) {
 
     //Get contents of response
     var response = event.Records[0].cf.response;
     var headers = response.headers;
 
-    let dataString = '';
-    const nav_bar = await new Promise((resolve, reject) => {
-        const req = https.get("https://assets.ctfassets.net/pz711f8blqyy/547b8bDM4xu8mCXujlZJpm/6744cd2b68566b4ac15c443033ab1423/homenav.txt", function(res) {
-          res.on('data', chunk => {
-            dataString += chunk;
-          });
-          res.on('end', () => {
-            resolve(dataString);
-          });
-        });
-    });
-
-    console.log(response);
-    console.log(response.body);
-    if(response.body) {
-        var output = response.body.replace(/NAV_BAR_PLACEHOLDER/g, nav_bar);
-        response.body = output;
-    }
     //Set new headers
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age= 63072000; includeSubdomains; preload'}];
     //headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}];
