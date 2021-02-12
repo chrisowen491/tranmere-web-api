777679f6e1fffc9e3e818426d4dd7fa95da2245c
chrisowen <chrisowen491@gmail.com>
2021-02-12 17:57:13 +0000

    updates

--- src/edge/app.js
+++ src/edge/app.js
@@ -1,5 +1,4 @@
-const request = require('axios');
-const AWSXRay = require('aws-xray-sdk');
+const https = require('https');
 
 exports.handler = async function (event, context) {
 
@@ -7,8 +6,19 @@ exports.handler = async function (event, context) {
     var response = event.Records[0].cf.response;
     var headers = response.headers;
 
-    var nav = await request.get('https://assets.ctfassets.net/pz711f8blqyy/547b8bDM4xu8mCXujlZJpm/6744cd2b68566b4ac15c443033ab1423/homenav.txt')
-    response.body = response.body.replace(/NAV_BAR_PLACEHOLDER/g, nav.data);
+    let dataString = '';
+    const nav_bar = await new Promise((resolve, reject) => {
+        const req = https.get("'https://assets.ctfassets.net/pz711f8blqyy/547b8bDM4xu8mCXujlZJpm/6744cd2b68566b4ac15c443033ab1423/homenav.txt", function(res) {
+          res.on('data', chunk => {
+            dataString += chunk;
+          });
+          res.on('end', () => {
+            resolve(dataString);
+          });
+        });
+    });
+
+    response.body = response.body.replace(/NAV_BAR_PLACEHOLDER/g, nav_bar);
 
     //Set new headers
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age= 63072000; includeSubdomains; preload'}];
